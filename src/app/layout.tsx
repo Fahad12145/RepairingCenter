812a60108a379bb1bcea7751ed9c081d7715c991
Fahad_Ali <fali08822@gmail.com>
2023-09-03 10:37:13 +0500

    google ads script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <title>Cool Repairing Center</title>
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7535988651402009"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <meta name="description" content="Cool & Cool Repairing Services " />
       <body>
         <LanguageProvider>{children}</LanguageProvider>
