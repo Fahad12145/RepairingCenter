2d7c079ac5d4fb7cf8656de46407e3fc5e116a3d
Abdullah Jalal <98460354+abdullahjalal85@users.noreply.github.com>
2025-06-02 21:31:47 +0500

    google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,12 +18,15 @@ export default function RootLayout({
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7535988651402009"
           crossOrigin="anonymous"
         ></script>
-
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TCHBX5CW');</script> 
+})(window,document,'script','dataLayer','GTM-TCHBX5CW');`,
+          }}
+        />
       </head>
       <meta name="description" content="Cool & Cool Repairing Services " />
       <body>
